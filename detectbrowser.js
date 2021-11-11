cd3db708f60802604a55d8c03af61ee0e26cad4b
Tom Moses <tom@tmoses.co.uk>
2021-11-11 13:17:32 +0000

    redirect if emoji domain supported

---
+++ detectbrowser.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isSafari) {
+        window.location.replace('https://my🌵.ml');
+    };
+});
